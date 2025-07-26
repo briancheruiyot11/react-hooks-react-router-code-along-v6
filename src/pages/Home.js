73f7adb686592ce161f104fd4a5a75ec1bc20f5c
Brian Cheruiyot <briancheru11@gmail.com>
2025-07-26 15:53:28 +0300

    Complete lab

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,30 @@
 import { useState, useEffect } from "react";
 import UserCard from "../components/UserCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
+      .then((r) => r.json())
+      .then(setUsers)
+      .catch((error) => console.error("Failed to fetch users:", error));
   }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>
-  });
 
   return (
     <>
       <header>
-        {/* place NavBar here */}
+        <NavBar />
       </header>
       <main>
         <h1>Home!</h1>
-        {userList}
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Home;
